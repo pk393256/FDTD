a749ae58e427ef9f6a3f99c531329d64e8d596d5
pk393256 <48841782+pk393256@users.noreply.github.com>
2019-03-30 02:49:01 +0530

    Update Simulation_of_a_sqare_object_kept_in_the_2D_electromagnetic_field.m

--- Simulation_of_a_sqare_object_kept_in_the_2D_electromagnetic_field.m
+++ Simulation_of_a_sqare_object_kept_in_the_2D_electromagnetic_field.m
@@ -18,78 +18,81 @@ for m=1:steps
         sigma(m,n)=0.009;
     end
 end
+%conductivity of space
 for m=10:90
     for n=10:90
     sigma(m,n)=0;
     end
 end
+%expression for loss
 for m=1:steps
     for n=1:steps
-        vise(m,n)=(sigma(m,n)*dt)/(2*epsilon);%loss for e field
+        vise(m,n)=(sigma(m,n)*dt)/(2*epsilon);
         
     end 
 end
-
+%for time steps 1 to 60
 for n = 1:60
 for l = 1: steps
     for m = 1:steps-1
-   Hx(m,l) = ((1-vise(l,m))/(1+vise(l,m))) .*Hx(m,l)-(1/(2*c0*miu.*(1+vise(l,m)))).*(Ez(m+1,l)-Ez(m,l));
+   Hx(m,l) = ((1-vise(l,m))/(1+vise(l,m))) .*Hx(m,l)-(1/(2*c0*miu.*(1+vise(l,m)))).*(Ez(m+1,l)-Ez(m,l));%magnetic field in x direction
     end
 end
 for p = 1: steps
     for q= 1:steps-1
-   Hy(p,q) =((1-vise(p,q))/(1+vise(p,q))) .* Hy(p,q)+(1/(2*c0*miu.*(1+vise(p,q)))).*(Ez(p,q+1)-Ez(p,q));
+   Hy(p,q) =((1-vise(p,q))/(1+vise(p,q))) .* Hy(p,q)+(1/(2*c0*miu.*(1+vise(p,q)))).*(Ez(p,q+1)-Ez(p,q));%magnetic field in y direction
     end
 end
-
+%electric in z direction
 for r = 2: steps
     for s = 2:steps
    Ez(r,s) =((1-vise(r,s))/(1+vise(r,s))) .* Ez(r,s)+(1/(2*c0*epsilon.*(1+vise(r,s)))).*(Hy(r,s)-Hy(r,s-1))-( 1/(2*c0*epsilon.*(1+vise(r,s)))).*(Hx(r,s)-Hx(r-1,s));
     end
 end
-Ez(88,88)=sin(((2*pi)/30)*n);
+Ez(88,88)=sin(((2*pi)/30)*n);%source
 
     
-
+%for body in the electric field
     for m=45:55  
     for n=45:55
         Ez(m,n)=0;
     end 
     end
     
- 
+ %creating the 3d movie plot
  mesh(x,y,Ez);
    axis([0 101 0 101 -2 2]);
     getframe;
 end
+%for time step after 60 
 for n = 61:T
 for l = 1: steps
     for m = 1:steps-1
-   Hx(m,l) = ((1-vise(l,m))/(1+vise(l,m))) .*Hx(m,l)-(1/(2*c0*miu.*(1+vise(l,m)))).*(Ez(m+1,l)-Ez(m,l));
+   Hx(m,l) = ((1-vise(l,m))/(1+vise(l,m))) .*Hx(m,l)-(1/(2*c0*miu.*(1+vise(l,m)))).*(Ez(m+1,l)-Ez(m,l));%magnetic field in x direction
     end
 end
 for p = 1: steps
     for q= 1:steps-1
-   Hy(p,q) =((1-vise(p,q))/(1+vise(p,q))) .* Hy(p,q)+(1/(2*c0*miu.*(1+vise(p,q)))).*(Ez(p,q+1)-Ez(p,q));
+   Hy(p,q) =((1-vise(p,q))/(1+vise(p,q))) .* Hy(p,q)+(1/(2*c0*miu.*(1+vise(p,q)))).*(Ez(p,q+1)-Ez(p,q));%magnetic field in y direction
     end
 end
-
+%electric field in z direction
 for r = 2: steps
     for s = 2:steps
    Ez(r,s) =((1-vise(r,s))/(1+vise(r,s))) .* Ez(r,s)+(1/(2*c0*epsilon.*(1+vise(r,s)))).*(Hy(r,s)-Hy(r,s-1))-( 1/(2*c0*epsilon.*(1+vise(r,s)))).*(Hx(r,s)-Hx(r-1,s));
     end
 end
-Ez(88,88)=0;
+Ez(88,88)=0;%source becomes 0
 
     
-
+%for body in electric field
     for m=45:55 
     for n=45:55
         Ez(m,n)=0;
     end 
     end
     
- 
+ %creating 3d movie plot
  mesh(x,y,Ez);
    axis([0 101 0 101 -2 2]);
     getframe;
